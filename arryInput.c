750ee7251a17c73a7886a95476195e6675682461
shamim-s <shamimibas@gmail.com>
2023-04-28 00:10:48 +0600

    first commit

---
+++ arryInput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    // int arr[] = {1,2,3,4,5,6,7,8};
+    // // for(int i = 0; i < 5; i++)
+    // // {
+    // //     scanf("%d", &arr[i]);
+    // // }
+    // printf("%d ", arr[1]);
+    int arrayName[] = {1, 2, 3};
+    printf("%d", arrayName[3]);
+    return 0;
+}
